f67135aafdea121d7afc6b40f22280fc5f206ed1
LeeAnna Rettke <leeanna.misterek@gmail.com>
2016-10-22 11:17:28 -0700

    New changes to foobooks including controllers and routes.

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use App\Http\Requests;
+class BookController extends Controller
+{
+    # /app/Http/Controllers/BookController.php
+
+
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return \App::environment();
+    #    return 'To do: Display a listing of all the books.';
+    }
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('book.create');
+    }
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        # Validate
+        $this->validate($request, [
+            'title' => 'required|min:3|alpha_num',
+        ]);
+        # If there were errors, Laravel will redirect the
+        # user back to the page that submitted this request
+        # If there were NO errors, the script will continue...
+        # Get the data from the form
+        #$title = $_POST['title']; # Option 1) Old way, don't do this.
+        $title = $request->input('title'); # Option 2) USE THIS ONE! :)
+        # Here's where your code for what happens next should go.
+        # Examples:
+        # Save book in the database
+        # When done - what should happen?
+        # You can return a String (not ideal), or a View, or Redirect to some other page:
+        return \Redirect::to('/books/create');
+        # FYI: There's also a Laravel helper that could shorten the above line to this:
+        # return redirect('/books/create');
+    }
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($title)
+    {
+        return view('book.show')->with('title', $title);
+    }
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        return 'To do: Show form to edit a book';
+    }
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+    /**
+	* This was example code I wrote in Lecture 7
+    * It shows, roughly, what a controller action for your P3 might look like
+    * It is not at all related to the Book resource.
+	*/
+    public function getLoremIpsumText(Request $request)
+    {
+        # Validate the request....
+        # Generate the lorem ipsum text
+        $howManyParagraphs = $request->input('howManyParagraphs');
+        # Logic...
+        $loremenator = \SBuck\Loremenator();
+        $text = $loremenator->getParagraphs($howManyParagraphs);
+        # Display the results...
+        return view('lorem')->with(['text', $text]);
+    }
+}
---
+++ app/Http/Controllers/PracticeController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class PracticeController extends Controller
+{
+
+    /**
+	*
+	*/
+    public function example5() {
+        $quote = new Quote();
+        return $quote->getRandomQuote();
+    }
+    /**
+	*
+	*/
+    public function example4() {
+        $random = new Random();
+        return $random->getRandomString(8);
+    }
+    /**
+	*
+	*/
+    public function example3() {
+        echo \App::environment();
+        echo 'App debug: '.config('app.debug');
+    }
+    /**
+	* Demonstrates useful data output methods like dump, and dd
+	*/
+    public function example2() {
+        $fruits = ['apples','oranges','pears'];
+        dump($fruits);
+        var_dump($fruits);
+        dd($fruits);
+    }
+    /**
+	*
+	*/
+    public function example1() {
+        return 'This is example 1';
+    }
+    /**
+	* Display an index of all available index methods
+	*/
+    public function index() {
+        # Get all the methods in this class
+        $actionsMethods = get_class_methods($this);
+        # Loop through all the methods
+        foreach($actionsMethods as $actionMethod) {
+            # Only if the method includes the word "example"...
+            if(strstr($actionMethod, 'example')) {
+                # Display a link to that method's route
+                echo '<a target="_blank" href="/practice/'.str_replace('example','',$actionMethod).'">'.$actionMethod.'</a>';
+            }
+        }
+    }
+}
+}
